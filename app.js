3ba4a53f6a1f51a2a69ef448f5b84cc28d1059c9
Jesse Woody <gearsighted@gmail.com>
2021-01-27 20:17:21 -0500

    New nav concept and added latest episodes and episode 1 page

--- app.js
+++ app.js
@@ -4,10 +4,29 @@ const header = document.querySelector('header');
 const body = document.querySelector('body');
 const nav = document.querySelector('nav');
 
-window.addEventListener('scroll', function() {  
-  if (window.scrollY > 0) {
+// window.addEventListener('scroll', function() {
+//   
+//   if (prevScrollpos > currentScrollPos) {
+//     header.classList.add('scrolled');
+//   } else {
+//     header.classList.remove('scrolled');
+//   }
+//   prevScrollpos = currentScrollPos;
+
+
+  window.addEventListener('scroll', function() {  
+  if (window.pageYOffset > 0) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
   }
 });
+
+// window.onscroll = scrollShowNav;
+// function scrollShowNav() {
+//    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//       header.style.top = "-100px";
+//    } else {
+//       header.style.top = "0px";
+//    }
+// }
