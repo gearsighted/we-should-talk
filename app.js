e64605acdedbcc6a3d4b1ef31188377f75d49dda
Jesse Woody <gearsighted@gmail.com>
2021-02-17 23:03:20 -0500

    multiple page template updates

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
-// Navbar effects on scroll
-
 const header = document.querySelector('header');
 const body = document.querySelector('body');
 const nav = document.querySelector('nav');
 
-// window.addEventListener('scroll', function() {
-//   
-//   if (prevScrollpos > currentScrollPos) {
-//     header.classList.add('scrolled');
-//   } else {
-//     header.classList.remove('scrolled');
-//   }
-//   prevScrollpos = currentScrollPos;
-
-
-  window.addEventListener('scroll', function() {  
-  if (window.pageYOffset > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
-
-// window.onscroll = scrollShowNav;
-// function scrollShowNav() {
-//    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//       header.style.top = "-100px";
-//    } else {
-//       header.style.top = "0px";
-//    }
-// }
