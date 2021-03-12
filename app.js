a39239140cb0a1f2c726633ff87b06983abea446
Jesse Woody <gearsighted@gmail.com>
2021-03-12 18:49:41 -0500

    fixed single ep link styles, tweaked footer(poorly)

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ const header = document.querySelector('header');
 const body = document.querySelector('body');
 const nav = document.querySelector('nav');
 
+
+// All pages:
+    // Navbar hide on scroll
+// Index.html:
+    // Hero Image Carousel
+    // Latest episode image focus effect
+    // 
+// Episodes.html:
+    // Hover effect on hero?
+    // Latest episode image focus effect
+// Episode1.html:
+    // // Hover effect on hero?    
+// About.html:
+    // Hover effect on hero?     
